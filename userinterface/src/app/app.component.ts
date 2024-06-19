a22ab919ed5bb5fd93c5ac6e4664ad07e2d725b9
abhayps1 <abhaypratapsingh1809@gmail.com>
2024-06-19 21:00:01 +0530

    Login Page Designed

--- userinterface/src/app/app.component.ts
+++ userinterface/src/app/app.component.ts
@@ -5,10 +5,13 @@ import { LoginPageComponent } from './login-page/login-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'userinterface';
+
+  constructor(){
+  }
 }
